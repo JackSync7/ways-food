e3e10d2f2f2db58c591825934eef67fc0b90c49d
jeri <jeriutama777abc@gmail.com>
2023-05-19 18:15:09 +0700

    fixing dashboard

--- client/src/components/reusable/CardProduct.jsx
+++ client/src/components/reusable/CardProduct.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function CardProduct(props) {
   return (
     <div>
-      <div className="w-fit px-4 text-lg h-16 gap-2 bg-white shadow-lg font-serif font-semibold flex justify-center items-center">
+      <div className="w-fit px-4 text-lg h-16 text-brownMain gap-2 bg-neutral-50 shadow-lg font-serif font-semibold flex justify-center items-center">
         <img className="w-12" src={props.img} alt="loading image" />
         {props.name}
       </div>
